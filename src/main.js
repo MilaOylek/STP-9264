7b9aefc375c23665674b34cb8ef6ed738c917b29
MilaOylek <oylekmila@gmail.com>
2025-05-31 09:41:52 +0300

    add footer html, privacy html

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-import './js/burgerMenu';
+// import './js/burgerMenu';
+
+// import Accordion from "accordion-js";
+// import "accordion-js/dist/accordion.min.css";
+
+// import Swiper from 'swiper';
+// import 'swiper/css';
+
+// import './js/faq.js';
+// import './js/mobile-menu.js';
+import './js/gallery.js';
+// import './js/review.js';
